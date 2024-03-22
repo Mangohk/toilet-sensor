a7cc22afa5160b63016841ae23c2fccbfaa04e72
Mango Man <mangohk@gmail.com>
2024-03-22 13:36:16 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,23 @@
-let Have_light = 0
-let Have_Sound = 0
+input.onButtonPressed(Button.A, function () {
+    A_Press = 1
+})
+input.onButtonPressed(Button.B, function () {
+    B_Press += 1
+})
+let B_Press = 0
+let A_Press = 0
 let Room_have_ppl_mins = 0
-loops.everyInterval(60000, function () {
-    if (Have_Sound) {
-        Have_Sound = 1
-    } else {
-        Have_Sound = 0
-    }
-    if (input.lightLevel() > 30) {
-        Have_light = 1
-    } else {
-        Have_light = 0
-    }
-    if (Have_light && Have_Sound) {
+let Active = 1
+loops.everyInterval(1000, function () {
+    if (input.lightLevel() > 50) {
         Room_have_ppl_mins += 1
     } else {
         Room_have_ppl_mins = 0
     }
+    if (!(Active)) {
+        Room_have_ppl_mins = 0
+    }
     basic.showNumber(Room_have_ppl_mins)
-})
-basic.forever(function () {
     if (Room_have_ppl_mins > 15) {
         music.setVolume(127)
         music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.LoopingInBackground)
